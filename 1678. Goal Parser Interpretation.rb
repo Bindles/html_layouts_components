b44a0e3a52350367a7e5a2173a925bf3e357ac08
Nicholas Turner <bindlez@gmail.com>
2024-05-06 22:28:44 -0400

    1678. Goal Parser Interpretation-RUBY

---
+++ 1678. Goal Parser Interpretation.rb
@@ -0,0 +1,39 @@
+command = "G()(al)"
+# @param {String} command
+# @return {String}
+#*
+def interpret(command)
+    command.gsub("()","o").gsub("(al)","al")
+end
+p interpret(command)
+
+#*
+def interpret(command)
+  result = ""
+  i = 0
+  
+  while i < command.size
+      if command[i] == "G"
+          result << command[i]
+      elsif command[i..i + 1] == "()"
+          result << "o"
+          i += 1
+      elsif command[i..i + 3] == "(al)"
+          result << "al"
+          i += 3
+      else
+          next
+      end
+      i += 1
+  end
+  result
+end
+p interpret(command)
+
+#* INTERSTING SOLUTIONS FORM OTHERS:
+#*
+def interpret(command)
+  { '()' => 'o', '(al)' => 'al' }.each { |k, v| command.gsub!(k, v) }
+  command
+end
+p interpret(command)
